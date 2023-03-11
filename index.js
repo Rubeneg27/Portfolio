c5917e5aa48ea255afacfdc9eb35fe859c428df1
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-03-11 19:16:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+/*
+OJO. Obligatorio fijar el tipo del script a "module" (ver index.html)
+El navegador tampoco deja importar archivos directamente desde nuestro PC. Es necesario tener los archivos en algún tipo de servidor
+*/
+
+// import PlatformGrass from "./Assets/PlatformGrass.png" //Por eso esta línea no funciona
+
+
+
+
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = window.innerWidth // Alteramos las propiedades de canvas con JS. Podríamos hacerlo con CSS
 canvas.height = 800
 
-const gravity = 0.4
+const gravity = 0.8
 
 class Player {
     constructor() {
         this.position = {
-            x: 50,
-            y: 0
+            x: 100,
+            y: 100
         }
         this.velocity = {
             x: 0,
@@ -28,21 +38,44 @@ class Player {
     update() { //Función para actualizar las propiedades de Player en función del tiempo
       this.draw()
       this.position.y += this.velocity.y // 100 + 0 = 100
-      this.position.x += this.velocity.x
-
-      if (this.position.y + this.height + this.velocity.y < canvas.height) { //Este loop para la caída tomando la altura del canvas. 100 + 40 + 0 <= altura de la pantalla? 
-        this.velocity.y += gravity // Si arriba = true -> 0 + 1.5
-      } else {
+      this.position.x += this.velocity.x // 50 + 0 = 50 Mientras no se actualice velocity.x, position.x seguirá siendo la misma
+      
+      if (this.position.y + this.height + this.velocity.y < canvas.height ) { //Este loop para la caída tomando la altura del canvas. Si 100 + 40 + 0 <= 800
+        this.velocity.y += gravity // Si arriba = true -> 0 + 0.4 = 0.4 + 0.4 = 0.8 + 0.4 ... etc, va acelerando
+        console.log(this.velocity.y)
+        console.log(this.position.y)
+      } else { 
+        this.position.y = 760
         this.velocity.y = 0 // Cuando lo de arriba = false -> velocity.y = 0. Es decir, al volver a llamar a update no cambiará this.position.y
       }
     } 
 }
 
+/*
+Constructor para UNA plataforma
+
 class Platform {
   constructor() {
     this.position = {
       x: 500,
-      y: 600
+      y: 600 
+    }
+    this.width = 200
+    this.height= 40
+  }
+
+  draw() {
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+}
+*/
+//Constructor para múltiples plataformas. Toma valores variables en lugar de los fijos del ejemplo anterior
+class Platform {
+  constructor(x,y) {
+    this.position = {
+      x: x,
+      y: y 
     }
     this.width = 200
     this.height= 40
@@ -54,8 +87,14 @@ class Platform {
   }
 }
 
-const Player1 = new Player()
-const Platform1 = new Platform()
+const Player1 = new Player() //Ojo a la sintaxis y a los paréntesis
+// const Platform1 = new Platform()
+const platforms = [ //En lugar de crear una sola plataforma, creamos un array de nuevas plataformas
+  new Platform(500, 600), //Cada una tiene definidos un valor x e y
+  new Platform(800, 400),
+  new Platform(1200, 200),
+  new Platform(3000, 300)
+] 
 
 const keys = {
   right: {
@@ -67,25 +106,52 @@ const keys = {
 
 }
 
+let scrollOffSet = 0 //Para definir el límite máximo de píxeles que se desplazarán los elementos (y definir por ejemplo el final del escenario)
 
 function animate() { //Actualiza cada segundo
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)//Cada segundo, limpiará el canvas 
   Player1.update() //update será llamado cada segundo, sumando la gravedad a la velocidad hasta que se cumpla la condicón del loop anterior
-  Platform1.draw()
+  platforms.forEach(platform => { //Para cada elemento dentro de platforms, se les asigna una constante platform y se dibuja
+    platform.draw()
+  })
+  //Platform1.draw() Esto sirve para una única plataforma
 
-  if (keys.right.pressed) {
-    Player1.velocity.x = 7
-  } else if (keys.left.pressed) {
-    Player1.velocity.x = -7
-  } else Player1.velocity.x = 0
+  if (keys.right.pressed && Player1.position.x + Player1.width < window.innerWidth - 200) { //Si presino derecha y el player no ha llegado al borde derecho...
+    Player1.velocity.x = 5
+  } else if (keys.left.pressed && Player1.position.x > 100) { //Si presiono izquierda y el player no ha llegado al borde izquierdo...
+    Player1.velocity.x = -5
+  } else { // Si estoy en algún borde
+    Player1.velocity.x = 0
 
-  if (Player1.position.y + Player1.height <= Platform1.position.y && 
-    Player1.position.y + Player1.height + Player1.velocity.y >= Platform1.position.y &&
-    Player1.position.x + Player1.width >= Platform1.position.x
-    ) { 
-    Player1.velocity.y = 0
-  } 
+    if (keys.right.pressed) { // Si estoy en los bordes y además pulso derecha...
+      scrollOffSet += 5 //Se actualiza el valor de scrollOfSet igual al de la velocidad al a que avanza la plataforma. Es decir, si se avanza, el scrollOfSet aumenta.
+      platforms.forEach(platform => {
+      platform.position.x -= 5
+      })
+    } else if (keys.left.pressed) { //si estoy en un borde y pulso izquierda...
+      scrollOffSet -= 5 //Disminuye el scrollOfSet si las plataformas se mueven a la izquierda (es decir, se retrocede o lo que es lo mismo te alejas del final)
+      platforms.forEach(platform => {
+        platform.position.x += 5
+      })
+    }
+  }
+
+  console.log(`Has avanzado ${scrollOffSet}`)
+
+  //Platform colission
+  platforms.forEach(platform => {
+    if (Player1.position.y + Player1.height <= platform.position.y && 
+      Player1.position.y + Player1.height + Player1.velocity.y >= platform.position.y &&
+      Player1.position.x + Player1.width >= platform.position.x && 
+      Player1.position.x <= platform.position.x + platform.width
+      ) { 
+      Player1.velocity.y = 0
+    } 
+  })
+  if (scrollOffSet > 2000) {
+    console.log('You win!')
+  }
 }
 
 //---MOVIMIENTO DEL PERSONAJE---
@@ -104,9 +170,9 @@ if ( keyCode == 37) {
 } else if ( keyCode == 39) {
   console.log('right')
   keys.right.pressed = true
-} else if ( keyCode == 38  ) {
+} else if ( keyCode == 38 && Player1.velocity.y == 0 ) {
   console.log('up')
-  Player1.velocity.y -= 14
+  Player1.velocity.y = -25
 }
 else if ( keyCode == 40) {
   console.log('down')
