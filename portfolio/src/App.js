75a7cac30c514593191d55151896506863bd7889
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-03-20 11:54:53 +0000

    sadf

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Nav from './components/nav'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
