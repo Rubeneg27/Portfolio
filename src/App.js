0b7a3db6e591965fd5de7597c54bfd3f8eca9de8
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-05-03 14:45:12 +0000

    ajustes de diseño

--- src/App.js
+++ src/App.js
@@ -9,13 +9,7 @@ function App() {
       <div className='Hero'>
       <Nav></Nav>
       <article>
-            Amante de la tecnología y el diseño gráfico. Graduado en Química en el año 2011 y trabajando de ello hasta el día de hoy. 
-            Aunque toda mi vida he sido una persona creativa, decidí dedicar mis esfuerzos académicos al ámbito de las ciencias experimentales. 
-            El interés por la tecnología es algo que también me ha acompañado desde que tengo uso de razón, motivo por el cual he acabado
-            interesándome por la programación. Siempre pensé que este tipo de trabajo no era para mí, que programar sería aburrido o demasiado difcícil. 
-            No fue hasta que di el paso y empecé a aprender Javascript que descubrí una pasión oculta por las líneas de código y una vía para desarrollar 
-            y reencontrarme con mi creatividad.
-
+            Presentación breve y fotografía
       </article>
       </div>
       
