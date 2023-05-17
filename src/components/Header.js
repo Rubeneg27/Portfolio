24ca898ad039b058f7a5c0e212d62669395ac3bb
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-05-17 14:17:31 +0000

    texto para el currículum

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+function Header () {
+    return (
+        <div className="Header">
+            <div className="cabecera">
+                <p className="title">Ruben's site</p>
+                <p>rubeneg27@gmail.com</p>
+            </div>
+            <div className="subCabecera">
+                <div className="logos">Aquí van logos</div>
+                <div className="resumen">Indicación del lugar actual en la página</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function Nav (props) {
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  console.log(`is Nav collapsed?` + isCollapsed)
+  const handleClick = () => {
+    setIsCollapsed(!isCollapsed)
+    props.onCollapseChange(true)
+  }
+  return (
+    <div>
+      <ul className={ isCollapsed ? "Nav-collapsed" : "Nav" } >
+      <li className={ isCollapsed ? "li-collapsed" : "li" } onClick={handleClick}>Home</li>
+      <li className={ isCollapsed ? "li-collapsed" : "li" } onClick={handleClick}>About</li>
+      <li className={ isCollapsed ? "li-collapsed" : "li" } onClick={handleClick}>Projects</li>
+      <li className={ isCollapsed ? "li-collapsed" : "li" } onClick={handleClick}>Contact</li>
+    </ul>
+    </div>
+    
+  )
+  
+}
+
+export default Nav;
