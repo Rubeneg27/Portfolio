c6fc7f726f8d2b1820b94308eb6e10e3255e93f3
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-05-23 07:10:57 +0000

    El logo lleva a home y maneja el colapso correctamente

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import './index.css';
 import Hero from './components/Hero'
 import Header from './components/Header';
+import Home from './components/Home'
 import { useState } from "react";
 
 function App() {
 
   const [isNavCollapsed, setIsNavCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false)
   const handleNavCollapseChange = () => {
-    setIsNavCollapsed(true);
+    setIsNavCollapsed(!isNavCollapsed);
+  }
+  const [content, setContent] = useState(
+    <Home></Home>
+  )
+  const updateContent = () => {
+    setContent(<Home></Home>)
+    setIsCollapsed(false)
+    setIsNavCollapsed(false)
   }
   
 
   return (
     <div className="App">
-      <Header isNavCollapsed={isNavCollapsed}></Header>
-      <Hero onCollapseChange={handleNavCollapseChange}></Hero>
+      <Header isNavCollapsed={isNavCollapsed} updateContent={updateContent}></Header>
+      <Hero onCollapseChange={handleNavCollapseChange} setContent={setContent} content={content} isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed}></Hero>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-function Header ({isNavCollapsed}) {
+function Header ({isNavCollapsed, updateContent}) {
     return (
         <div className="Header">
             <div className="cabecera">
-                <p className="title">Ruben's site</p>
+                <p className="title" onClick={updateContent}>Ruben's site</p>
                 <p>rubeneg27@gmail.com</p>
             </div>
             <div className="subCabecera">
