06c42f005ab3ea6ef64dfd7c75816cddbbe9d87c
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-06-19 13:21:17 +0000

    esctructurando skillks

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -1,29 +1,35 @@
 function Skills() {
     return (
-        <section>
-					<h2>Formación académica</h2>
-					<p>Graduado en Química por la Universidad de Sevilla</p>
-					<h2>Habilidades</h2>
+        <section className="skills">
+            <div className="skills-a">
+                <div>
+                    <h2>Formación académica</h2>
+				    <p>Graduado en Química por la Universidad de Sevilla</p>
+                </div>
+                <div>
+                    <h2>Habilidades</h2>
                     <h3>Programación</h3>
-					<p>META Front-end Developer.</p>
-					<p>
-					Certificado constituido por 9 cursos:<br></br>
-        HTML and CSS in depth<br></br>
-        Introduction to Front-End Development<br></br>
-        Front-End Developer Capstone<br></br>
-        Programming with JavaScript<br></br>
-        Version Control<br></br>
-        Coding Interview Preparation<br></br>
-        React Basics<br></br>
-        Principles of UX/UI Design<br></br>
-        Advanced React<br></br>
+				    <p>Certificado Meta Front-end Developer.</p>
+                </div>
+                <div>
                     <h3>Idiomas</h3>
-                    Inglés - Certificado B2 - Nivel conversacional
+                    Inglés - Certificado B1 - Nivel conversacional
+                </div>            
+                </div>
+            <div className="skills-b">
+                <div className="skills-img">
+
+                </div>
+                <div>
                     <h3>Sociales</h3>
+                    <p>
                     Habituado al trabajo en equipo. No tengo problemas para aportar mi punto de vistar ni escuchar el de los demás.<br></br> 
                     Siempre dispuesto a aprender y sin miedo a reconocer si estoy equivocado.<br></br>
                     Buena actitud ante la presión afrontándola con paciencia y la perseverancia.
-					</p>
+				    </p>
+                </div>
+                
+            </div>               
 				</section>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,22 @@
 function Home () {
   return (
     <section className="Home">
-      <div>
-      <p>
-        Soy Rubén Espinosa González
-      </p>
-      <p>
-        Entusiasta del diseño y la tecnología
-      </p>
-      <p>
-        En mi sitio personal podrás conocer mi formación, explorar mis proyectos y contactarme.
-      </p>
-      <p>
-        ¡Espero que disfrutes la visita!
-      </p>
-      </div>
-      <img alt="">
-      </img>
+      <div className="Home-text">
+        <p>
+        Soy<br></br> 
+        <p >Rubén Espinosa González</p>
+        </p>
+        <p>
+          Entusiasta del diseño y la tecnología
+        </p>
+        <p>
+          En mi sitio personal podrás conocer mi formación, explorar mis proyectos y contactarme.
+        </p>
+        <p>
+          ¡Espero que disfrutes la visita!
+        </p>
+        </div>
+      <img alt="" />
     </section>
     
   )
