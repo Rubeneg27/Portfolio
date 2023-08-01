be616ef0f22693d8668b420b8b0e2cb0e4c41909
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-08-01 08:28:47 +0000

    eliminada clase subcabecera innecesaria

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,16 +3,12 @@ import {FaReact, FaJs, FaHtml5, FaCss3 } from "react-icons/fa";
 function Nav ({isNavCollapsed, isCollapsed, handleClickHome, handleClickAbout, handleClickProjects, handleClickContact}) {
   return (
       <ul className={ isCollapsed ? "Nav-collapsed" : "Nav" } >
-
-        <div className="subCabecera">
                 <div className={isNavCollapsed? "logos-collapsed" : "logos"}>
                     <FaJs className="logo" />
                     <FaReact className="logo" />
                     <FaHtml5 className="logo" />
                     <FaCss3 className="logo" />
                 </div>
-            </div>
-
         <li className={ isCollapsed ? "li-collapsed" : "li"} onClick={handleClickHome}>Home</li>
         <li className={ isCollapsed ? "li-collapsed" : "li"} onClick={handleClickAbout}>About</li>
         <li className={ isCollapsed ? "li-collapsed" : "li"} onClick={handleClickProjects}>Projects</li>
