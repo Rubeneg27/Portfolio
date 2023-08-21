82d95d07b88e9843b55529478a1dee82a433a42b
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-08-21 12:20:24 +0000

    reorganizado header y logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,14 @@ import {FaReact, FaJs, FaHtml5, FaCss3 } from "react-icons/fa";
 function Header ({isNavCollapsed, updateContent}) {
     return (
         <div className="Header">
-            <div className="cabecera">
                 <div className="title" onClick={updateContent}>Ruben's site</div>
-                <div className="decoration"></div>
-                <div className="logos">
-                    <FaJs className="logo" />
-                    <FaReact className="logo" />
-                    <FaHtml5 className="logo" />
-                    <FaCss3 className="logo" />
+                <div className="decoration">
+                        <FaJs className="logo" />
+                        <FaReact className="logo" />
+                        <FaHtml5 className="logo" />
+                        <FaCss3 className="logo" />
                 </div>
-                <div className="email">rubeneg27@gmail.com</div>
-            </div>
-            
+                <div className="email">rubeneg27@gmail.com</div> 
         </div>
     )
 }
