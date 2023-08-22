c77c6aa6cd5ae8b546237fcb5e3bcd9b44811d3d
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-08-22 07:24:12 +0000

    estilo logos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import {FaReact, FaJs, FaHtml5, FaCss3 } from "react-icons/fa";
+import {FaReact, FaJs, FaHtml5, FaCss3, FaUnity } from "react-icons/fa";
 
 function Header ({isNavCollapsed, updateContent}) {
     return (
@@ -9,6 +9,8 @@ function Header ({isNavCollapsed, updateContent}) {
                         <div className="logoCont2"><FaReact className="logo2" /><span>React</span></div>
                         <div className="logoCont3"><FaHtml5 className="logo3" /><span>HTML5</span></div>
                         <div className="logoCont4"><FaCss3 className="logo4" /><span>CSS</span></div>
+                        <div className="logoCont5"><FaUnity className="logo5" /><span>Unity</span></div>
+                        <div className="logoCont6"><logo className="logo6" >C#</logo><span>C#</span></div>
                 </div>
                 <div className="email">rubeneg27@gmail.com</div> 
         </div>
