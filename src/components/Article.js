cfc2dd14090a686275ba27aec30b2ddd7cd01ca8
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-10-11 14:11:30 +0000

    Burger provisional para móvil

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,10 +3,13 @@ import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import Burger from "./Burger";
+import { useDevice } from "./Context/DeviceContext.js";
 
 
 function Article ({content, isCollapsed, handleClick, isMenuOpen, toggleMenu, handleCloseGame, isGameClosed, handleCollapse, showButtons, setShowButtons}) {
 
+  const { isMobile } = useDevice();
+  
   return (
     <div className={isCollapsed? "article-expanded" : "article"}>
     <Burger isCollapsed={isCollapsed} isMenuOpen={isMenuOpen} handleClick={handleClick} toggleMenu={toggleMenu}></Burger>
--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,17 +1,21 @@
+import { useDevice } from "./Context/DeviceContext.js";
+
 function Burger ({isCollapsed, isMenuOpen, handleClick, toggleMenu}) {
+
+  const { isMobile } = useDevice();
   
   return (
     <div>
-      {isCollapsed?
+      {isCollapsed || isMobile ?
       <div>
-        <div className={isMenuOpen? "burger-hidden": "burger" }onClick={toggleMenu} >
+        <div className={isMenuOpen? "burger-hidden": isMobile ? "burgerMobile" : "burger" }onClick={toggleMenu} >
           <svg width="60px" height="60px">
             <rect></rect>
             <rect y="10"></rect>
             <rect y="20"></rect>
           </svg>
         </div>
-        <ul className={isMenuOpen? "dropdown-menu" : "dropdown-menu-hidden"}>
+        <ul className={isMenuOpen ? "dropdown-menu" : isMenuOpen && isMobile ? "dropdownMenuMobile": "dropdown-menu-hidden"}>
           <li onClick={()=>{handleClick("Home")}}>Home</li>
           <li onClick={()=>{handleClick("About")}}>About</li>
           <li onClick={()=>{handleClick("Contact")}}>Contact</li>
