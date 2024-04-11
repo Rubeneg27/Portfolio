c5d169ebf46378856c13f1bdd91f62ffb95bb74f
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2024-04-11 13:04:49 +0000

    comentadas advertencias de compilación

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -5,12 +5,12 @@ import About from "../About/About";
 import Projects from "../Projects/Projects.js";
 import Contact from "../Contact/Contact";
 import Burger from "../Burger/Burger";
-import { useDevice } from "../Context/DeviceContext.js";
+// import { useDevice } from "../Context/DeviceContext.js";
 
 
 function Article ({content, isCollapsed, handleClick, isMenuOpen, toggleMenu, handleCloseGame, isGameClosed, handleCollapse, showButtons, setShowButtons}) {
 
-  const { isMobile } = useDevice();
+  // const { isMobile } = useDevice();
   
   return (
     <div className={isCollapsed? "article-expanded" : "article"}>
