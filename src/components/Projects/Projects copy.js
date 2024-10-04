2f8076dadf52c7dd076fd9da8659449ae4b3f668
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2024-10-04 13:35:35 +0000

    prueba dominio

---
+++ src/components/Projects/Projects copy.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import './css/Projects.css'
+import Platformer from "./Platformer.js"
+import ShootEmUp from "./ShootEmUp/ShootEmUp.js";
+/*import TestFPS from "./Projects/Testing.js";*/
+
+
+function Projects ({handleCollapse, isGameClosed, handleCloseGame}) {
+
+  const [project, setProject] = useState("");
+  const [pressed, setPressed] = useState(false);
+
+  function handleClick (elemento) {
+    setPressed(true)
+    handleCollapse(true)
+    switch(elemento) {
+      case "Platformer":
+        setProject("platformer")
+        break;
+        case "ShootEmUp":
+          setProject("shootemup")
+          break;
+      default:
+        setProject("")
+    }
+  }
+
+    return (
+        <section className="projects">
+          {pressed ? null : 
+            <div>
+              <section className="button-container">
+              {pressed ? null : (<button onClick={()=>handleClick("Platformer")}>Platformer</button>)}
+              <div className="resume"></div>
+            </section>
+            <section className="button-container">
+              {pressed ? null : (<button onClick={()=>handleClick("ShootEmUp")}>ShootEmUp</button>)}
+              <div className="resume"></div>
+            </section>
+            </div>
+            }
+          {project === "shootemup" ? <ShootEmUp setProject={setProject} setPressed={setPressed} handleCollapse={handleCollapse} handleCloseGame={handleCloseGame} isGameClosed={isGameClosed}></ShootEmUp> : null}
+          {project === "platformer" ? <Platformer setProject={setProject} setPressed={setPressed} handleCollapse={handleCollapse} handleCloseGame={handleCloseGame} isGameClosed={isGameClosed}></Platformer> : null}
+        </section>
+        
+    )
+}
+
+export default Projects;
