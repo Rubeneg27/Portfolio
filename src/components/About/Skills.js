7f5820a60ea6a731f4b93cf5ba1a347a96762bd8
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2024-10-24 13:50:40 +0000

    refactorizado skills

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -21,45 +21,92 @@ const Skills = forwardRef(({}, ref) => {
         setShowCertificateImg(e);
     }
 
+    const academicBackground = [
+        {
+            title: "Bootcamp- Angular Front-End Developer",
+            institution: "University of La Rioja/Qualentum (Grupo Proeduca)"
+        },
+        {
+            title: "Certification: Front-end Developer",
+            institution: "Meta - Coursera",
+            certificateLink: "https://www.coursera.org/account/accomplishments/professional-cert/N6SAE74TJBAN"
+        },
+        {
+            title: "Graduated in Chemistry",
+            institution: "University of Seville"
+        }
+    ];
+
+    const languages = [
+        {
+            language: "English",
+            certification: "Certification: B1.",
+            speakingLevel: "Speaking Level: Conversational."
+        },
+        {
+            language: "Spanish",
+            certification: "Native"
+        }
+    ];
+
+    const techStack = [
+        "HTML5, CSS",
+        "Javascript, Typescript, C#, Python",
+        "React, Angular",
+        "Git, GitHub",
+        "Unity, Photoshop, Blender, Adobe Substance Painter"
+    ];
+
+    const certificates = [
+        {
+            title: "Coursera-frontend",
+            link: "https://www.coursera.org/account/accomplishments/professional-cert/N6SAE74TJBAN",
+        }  
+    ]
+
     return (
         <section ref={skillsRef} className={isMobile ? "skillsMobile" : "skills"}>
-            <a hidden={!showCertificateImg} href="https://www.coursera.org/account/accomplishments/professional-cert/N6SAE74TJBAN" target="blank"></a>
+            {certificates.map((certtificate, index) => (<a hidden={!showCertificateImg} href={certtificate.link} target="blank"></a> ))}
             <div className={showCertificateImg ? "CertificateImg" : "CertificateImg_hidden"}>
                 {<img alt="certificado coursera de front end developer" src={courseraCertificate}></img>}
             </div>
             <div className='popUpText'>
                 <div className="dashCard">ACADEMIC BACKGROUND</div>
                 <div className="dashCard">
-                    <p className='popUpText'>Bootcamp- Angular Front-End Developer</p>
-                    <p>University of La Rioja/Qualentum (Grupo Proeduca)</p>
-                    <p className='popUpText'>Certification: <span className="customLink" onClick={() => handleCertificateImg(true)}>Front-end Developer</span>.</p>
-                    <p>Meta - Coursera</p>
-                    <p className='popUpText'>Graduated in Chemistry</p>
-                    <p>University of Seville</p>
+                    {academicBackground.map((item, index) => (
+                        <div key={index}>
+                            <p className='popUpText'>{item.title}</p>
+                            <p>{item.institution}</p>
+                            {item.certificateLink && (
+                                   <span className="customLink" onClick={() => handleCertificateImg(true)}>Certificate</span>
+                            )}
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className='popUpText'>
                 <div className="dashCard">LANGUAGES</div>
                 <div className="dashCard">
-                    <h3 className='popUpText'>English</h3>
-                    <p>Certification: B1.</p>
-                    <p>Speaking Level: Conversational.</p>
-                </div>
-                <div className="dashCard">
-                    <h3 className='popUpText'>Spanish</h3>
-                    <p>Native</p>
+                    {languages.map((lang, index) => (
+                        <div key={index}>
+                            <h3 className='popUpText'>{lang.language}</h3>
+                            <p>{lang.certification}</p>
+                            {lang.speakingLevel && <p>{lang.speakingLevel}</p>}
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className='popUpText'>
                 <div className="dashCard">TECH STACK</div>
                 <div className="dashCard">
-                    <p>HTML5, CSS</p>
-                    <p>Javascript, Typescript, C#, Python</p>
-                    <p>React, Angular</p>
-                    <p>Git, GitHub</p>
-                    <p>Unity, Photoshop, Blender, Adobe Substance Painter</p>
+                    <ul>
+                        {techStack.map((tech, index) => (
+                                <li key={index}>{tech}</li>
+                        ))}
+                    </ul>
                 </div>
             </div>
+
             {showCertificateImg ? <div className="imgClose" onClick={() => handleCertificateImg(false)}></div> : null}
         </section>
     );
